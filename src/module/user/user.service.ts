cea03a3f279a5db8a6e7147b4708703d3850f2d1
Edward-Brock <1416477069@qq.com>
2023-09-12 22:56:56 +0800

    添加相关注释，更新 user 模块中的 update 及 remove 方法；

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -14,17 +14,21 @@ export class UserService {
   ) {}
 
   async create(createUserDto: CreateUserDto) {
+    // 查找数据库中是否包含当前账号信息
     const findOneResult = await this.siteEntityRepository.findOneBy({
       username: createUserDto.username,
     });
+    // 判断查找到的信息是否为 null，不为 null 则报错，为 null 则可以进行注册
     if (findOneResult !== null) {
-      console.error(`用户 ${createUserDto.username} 已存在，请更换重试`);
+      console.error(`账号名称 ${createUserDto.username} 已存在，请更换重试`);
       throw new HttpException(
-        `用户 ${createUserDto.username} 已存在，请更换重试`,
+        `账号名称 ${createUserDto.username} 已存在，请更换重试`,
         HttpStatus.CREATED,
       );
     }
+    // 生成盐
     const salt = await bcrypt.genSalt();
+    // 将 createUserDto 中的密码字段替换为已加密的密码字段
     createUserDto.password = await bcrypt.hash(createUserDto.password, salt);
     return this.siteEntityRepository.save(createUserDto);
   }
@@ -32,6 +36,7 @@ export class UserService {
   findAll() {
     return this.siteEntityRepository.find({
       where: {
+        // 排除删除标记为 true 的数据
         isDelete: false,
       },
     });
@@ -41,11 +46,17 @@ export class UserService {
     return this.siteEntityRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      // 生成盐
+      const salt = await bcrypt.genSalt();
+      // 将 createUserDto 中的密码字段替换为已加密的密码字段
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, salt);
+    }
     return this.siteEntityRepository.update(id, updateUserDto);
   }
 
   remove(id: number) {
-    return this.siteEntityRepository.delete(id);
+    return this.update(id, { isDelete: true });
   }
 }
