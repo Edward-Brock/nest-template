9ab857255694ef382907b37df72d5dd597f74194
Edward-Brock <1416477069@qq.com>
2023-09-18 22:13:05 +0800

    添加 role 权限类型； 针对全局异常过滤器进行优化； 对全局响应拦截器格式化代码；

--- src/middleware/filter/uinify-exception.filter.ts
+++ src/middleware/filter/uinify-exception.filter.ts
@@ -4,6 +4,7 @@ import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { getReqMainInfo } from '../../utils/getReqMainInfo';
 import * as dayjs from 'dayjs';
+import { HttpAdapterHost } from '@nestjs/core';
 
 /**
  * 全局异常捕获过滤器
@@ -13,39 +14,41 @@ export default class UnifyExceptionFilter implements ExceptionFilter {
   // 注入日志服务相关依赖
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    private readonly httpAdapterHost: HttpAdapterHost,
   ) {}
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
     const ctx = host.switchToHttp(); // 获取当前执行上下文
     const res = ctx.getResponse<Response>(); // 获取响应对象
     const req = ctx.getRequest<Request>(); // 获取请求对象
-    const status =
+    const httpStatus =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
     const message =
-      exception.message ||
-      exception.message['message'] ||
-      exception.message['error'] ||
+      exception['message'] ||
+      exception['message']['message'] ||
+      exception['message']['error'] ||
       null;
 
-    const data = {
-      statusCode: status,
+    const responseBody = {
+      statusCode: httpStatus,
       timestamp: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-      path: req.url,
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
       data: message,
     };
 
     // 记录日志（错误消息，错误码，请求信息等）
     this.logger.error(`⚠️ 全局异常过滤器已捕获到异常 ⚠️`);
     this.logger.error(message, {
-      status,
+      httpStatus,
       req: getReqMainInfo(req),
-      stack: exception.stack,
+      stack: exception['stack'],
     });
 
     res
-      .status(status >= 500 ? status : 200)
-      .json({ code: 1, data, msg: 'ERROR' });
+      .status(httpStatus >= 500 ? httpStatus : 200)
+      .json({ code: 1, responseBody, msg: 'ERROR' });
   }
 }
--- src/module/role/role.enum.ts
+++ src/module/role/role.enum.ts
@@ -1,4 +1,5 @@
 export enum Role {
-  User = 'user',
-  Admin = 'admin',
+  User = 'user', // 普通用户权限
+  System = 'system', // 系统用户权限
+  Admin = 'admin', // 管理用户权限
 }
